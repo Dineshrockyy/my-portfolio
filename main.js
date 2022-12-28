617a7c0400fd96601f61bea8e46428b757f61b96
Dineshrockyy <dineshbabu6369@gmail.com>
2022-12-28 04:05:40 -0800

    form added

--- main.js
+++ main.js
@@ -11,5 +11,35 @@ const handelmenubar=()=>{
 const handelcancelbar=()=>{
     menu.classList.add("hidden")
     cancel.classList.add("hidden")
-    toggel.classList.remove("hidden")
+    toggel.classList.remove("hidden ")
 }
+
+function sendmail(){
+    const params={
+       name:document.getElementById("name").value,
+       email:document.getElementById("email").value,
+       message:document.getElementById("text").value,
+    }
+
+    const service_id="service_3y48ao8";
+    const template_id="template_48ngd7h";
+
+    if(params.name==="" || params.email==="" || params.message===""){
+        document.getElementById("denied").classList.remove("hidden")
+        document.getElementById("success").classList.add("hidden")
+    }else{
+        emailjs.send(service_id,template_id,params)
+.then(
+    res =>{
+        document.getElementById("name").value=""
+        document.getElementById("email").value=""
+        document.getElementById("text").value=""
+        document.getElementById("success").classList.remove("hidden")
+        document.getElementById("denied").classList.add("hidden")
+        console.log(res)
+
+    }
+)
+.catch((err)=>{console.log(err)})
+}
+    }
